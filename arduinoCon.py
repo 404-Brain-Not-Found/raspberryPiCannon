e93a6ed7a17819f8e1b11b90adb5aa74a46a2b91
custer259 <thom.quirk180@gmail.com>
2016-02-29 15:01:31 -0500

    what did it do?

--- arduinoCon.py
+++ arduinoCon.py
@@ -1,16 +1,9 @@
-import smbus
-import time
-
-bus = smbus.SMBus(1)
-
-ARDUINOADDRESS = 0x04
-
 def writeNumber(value):
     bus.write_byte(ARDUINOADDRESS, value)
     return -1
 
 def readNumber():
     return bus.read_byte(ARDUINOADDRESS)
-def getGyroVaule():
+def getGyroValue():
     writeNumber(True)
     return readNumber()
