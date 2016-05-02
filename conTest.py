5e225b02353d4da16c059e3ffc934922bf5d8a48
thom quirk <thom quirk>
2016-05-02 13:45:02 -0500

    test program for con link

--- conTest.py
+++ conTest.py
@@ -1,3 +1,13 @@
-import arduinoCon
-while True:
-    print getGyroValue()
+import RPi.GPIO as GPIO
+import time
+
+pin = 5
+GPIO.setup(pin, GPIO.OUT)
+GPIO.output(pin, GPIO.LOW)
+count = 0
+time.sleep(.1)
+
+GPIO.setup(pin, GPIO.IN)
+while GPIO.input(pin) == GPIO.LOW:
+    count += 1
+print count
